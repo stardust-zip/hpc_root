267f88f6d364f6b102985e83df6a66af2493ae9f
stardust-zip <nguyenhieu0802@proton.me>
2026-03-16 09:06:56 +0700

    build: update local docker compose

--- test_script.sh
+++ test_script.sh
@@ -1,15 +1,12 @@
-printf "Welcome to TEST SCRIPT with cURL\n"
+path="http://localhost:8082/api/v1/login"
+jwt="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjcsInVzZXJfdHlwZSI6InN0dWRlbnQiLCJ1c2VybmFtZSI6Im5nb2NfaGlldSIsImlzX2FkbWluIjpmYWxzZSwiZW1haWwiOiJuZ29jaGlldUB0ZXN0LmNvbSIsImZ1bGxfbmFtZSI6Ik5ndXlcdTFlYzVuIE5nXHUxZWNkYyBIaVx1MWViZnUiLCJkZXBhcnRtZW50X2lkIjpudWxsLCJjbGFzc19pZCI6MSwiaWF0IjoxNzczMzI1MTY2LCJleHAiOjE3NzMzMjg3NjZ9.f6scyWegl6TPp8tPl0cITEYaB9RdIkdnzX8rZD3FKF8"
 
-while true; do
-	read -p "Enter HTTP Method (GET/POST/PUT/DELETE/PATCH) [Default=GET]: " METHOD
-	METHOD=${METHOD:-GET}
 
-	# Uppercase
-	METHOD=${METHOD^^}
-	
-	case "$METHOD" in
-		GET|POST|PUT|DELETE|PATCH) break ;;
-		*) printf "Invalid method. Please try again.\n" ;;
-	esac 
-done
+curl -X POST "$path" \
+-H "Content-Type: application/json" \
+-d '{
+    "username": "ngoc_hieu",
+    "password": "123456",
+    "user_type": "student"
+}' | jq
 
