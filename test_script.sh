32a75954e8a20e4d9fbb3234bfc7530cb1bf7ab5
stardust-zip <nguyenhieu0802@proton.me>
2026-03-17 20:33:59 +0700

    update hpc_drive

--- test_script.sh
+++ test_script.sh
@@ -1,12 +1,14 @@
-path="http://localhost:8082/api/v1/login"
-jwt="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjcsInVzZXJfdHlwZSI6InN0dWRlbnQiLCJ1c2VybmFtZSI6Im5nb2NfaGlldSIsImlzX2FkbWluIjpmYWxzZSwiZW1haWwiOiJuZ29jaGlldUB0ZXN0LmNvbSIsImZ1bGxfbmFtZSI6Ik5ndXlcdTFlYzVuIE5nXHUxZWNkYyBIaVx1MWViZnUiLCJkZXBhcnRtZW50X2lkIjpudWxsLCJjbGFzc19pZCI6MSwiaWF0IjoxNzczMzI1MTY2LCJleHAiOjE3NzMzMjg3NjZ9.f6scyWegl6TPp8tPl0cITEYaB9RdIkdnzX8rZD3FKF8"
+path="http://localhost:8888/dispatches/10/assign"
+jwt="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjMsInVzZXJfdHlwZSI6ImxlY3R1cmVyIiwidXNlcm5hbWUiOiJ0aGF5X2h1bmciLCJpc19hZG1pbiI6ZmFsc2UsImVtYWlsIjoidGhheWh1bmdAc3lzdGVtLmNvbSIsImZ1bGxfbmFtZSI6IkhvXHUwMGUwbmcgTmdcdTFlY2RjIEhcdTAxYjBuZyIsImRlcGFydG1lbnRfaWQiOjEsImNsYXNzX2lkIjpudWxsLCJpYXQiOjE3NzM3NTE5ODYsImV4cCI6MTc3Mzc1NTU4Nn0.rJKBq-WapucTU9n1ZsIvWOV_uYBw5eqCBc64-9CRkqE"
 
 
 curl -X POST "$path" \
 -H "Content-Type: application/json" \
+-H "Authorization: Bearer $jwt" \
 -d '{
-    "username": "ngoc_hieu",
-    "password": "123456",
-    "user_type": "student"
+	"assignee_usernames": [
+		"thay_tuan_anh"
+	],
+	"action_required": "Nothing"
 }' | jq
 
